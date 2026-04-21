6218f3aa9ae4ce9ff6b3f55cd02b6460c227a1c9
vicek <jesseymoore2007@gmail.com>
2026-04-21 16:03:50 -0500

    Pretty much completed Minesweeper. I have the timer, and it displays the top three times.

--- logic.py
+++ logic.py
@@ -1,35 +1,42 @@
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer, QTime, Qt
 from PyQt6.QtWidgets import *
 from gui import *
 import random
+import csv
+
+#End screen
+#reset button
+#Track best times
+
 
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
+        #Basic initiations and variables
         super().__init__()
         self.setupUi(self)
         self.Win_Label.hide()
-        self.__flags:int = 3
-        self.__time:int = 0
+        self.page.showFullScreen()
+        self.__flags:int = 3 #The amount of "flags" or mines
+        self.time = QTime(0,0,0,0)
+        self.timer = QTimer()
         self.__running:bool = True
+        self.__timer_running:bool = False
 
+        #set up grid in a list so I can later track mine positions and clicked locations
         self.grid_rows:list = ["A", "B", "C", "D", "E"]
         self.grid_columns:list = ["1", "2", "3", "4", "5"]
         self.grid:list = [[],[],[],[],[]]
-        self.mines:list = []
-        self.cleared:int = 0
-        self.safe_tiles:int = 22
-        self.tiles_cleared:dict = {}
-        self.Flags_Label.setText(f"Flags: {self.__flags}")
 
-        first = True
-        for button in self.gridLayoutWidget.children():
-            if first:
-                first = False
-                continue
-            button.clicked.connect(lambda: self.click())
-            button.setStyleSheet("background-color: tan")
+        self.mines:list = [] #Which tiles are mines
+        self.cleared:int = 0 #How many tiles are currently cleared
+        self.safe_tiles:int = 22 #The amount of tiles that are safe based on the fact there are 3 mines (25 - 3)
+        self.tiles_cleared:dict = {} #A dictionary to track which tile positions have already been clicked or not
 
+        self.Flags_Label.setText(f"Mines: {self.__flags}")
         self.create_grid()
+        self.Reset_Button.clicked.connect(lambda: self.create_grid())
+
+        self.timer.timeout.connect(self.update_timer)
 
 
 
@@ -38,6 +45,24 @@ class Logic(QMainWindow, Ui_MainWindow):
         Sets up the grid lists and the mine locations based on the grid
         :return:
         '''
+        self.time = QTime(0,0,0,0)
+        self.Timer_Label.setText(f"Time: 00:00:00")
+        self.__running = True
+        self.page_2.hide()
+        self.page.showFullScreen()
+        self.tiles_cleared.clear()
+        self.cleared = 0
+
+        first = True
+        for button in self.gridLayoutWidget.children():
+            if first:
+                first = False
+                continue
+            button.clicked.connect(lambda: self.click())
+            button.setText("")
+            button.setStyleSheet("background-color: tan")
+
+
         letter = 0
         for row in self.grid:
             for col in range(5):
@@ -61,6 +86,7 @@ class Logic(QMainWindow, Ui_MainWindow):
         print(self.tiles_cleared)
 
 
+
     def click(self) -> None:
         '''
         Definately the most complex function here.
@@ -70,6 +96,8 @@ class Logic(QMainWindow, Ui_MainWindow):
         :return:
         '''
         if self.__running:
+            if not self.__timer_running:
+                self.timer.start(10)
             ##Set name of the button, and location of the tile on the grid
             button = self.sender()
             tile = button.objectName()
@@ -162,6 +190,34 @@ class Logic(QMainWindow, Ui_MainWindow):
         Will make a cool little win text show up.
         :return:
         '''
+        self.timer.stop()
+        self.__timer_running = False
+        self.page_2.showFullScreen()
+        self.page.hide()
+        self.Times_Label.setGeometry(QtCore.QRect(25, 60, 250, 20))
+        self.Times_Label.setText(f"Your Time: {self.time.hour():02}:{self.time.minute():02}:{self.time.second():02}")
+        with open("best_times.csv", "a", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow([f"{self.time.hour():02}:{self.time.minute():02}:{self.time.second():02}"])
+        with open("best_times.csv", "r", newline="") as csvfile:
+            csv_reader = csv.reader(csvfile)
+            first = None
+            second = None
+            third= None
+            for time in csv_reader:
+                if first == None:
+                    first = time[0]
+                    continue
+                if time[0] < first:
+                    third = second
+                    second = first
+                    first = time[0]
+                elif time[0] < second:
+                    third = second
+                    second = time[0]
+                elif time[0] < third:
+                    third = time[0]
+        self.Best_Times_Label.setText(f"Top 3 Times:\n{first:>12}\n{second:>12}\n{third:>12}")
         self.Win_Label.show()
         self.Win_Label.setStyleSheet("color: green")
         self.Win_Label.setText("Win!")
@@ -174,7 +230,37 @@ class Logic(QMainWindow, Ui_MainWindow):
         (It's still Win_Label, but nobody will know...)
         :return:
         '''
+        self.timer.stop()
+        self.__timer_running = False
+        self.page_2.showFullScreen()
+        self.page.hide()
+        self.Times_Label.setGeometry(QtCore.QRect(15, 60, 250, 20))
+        self.Times_Label.setText(f"You lost! No time saved!")
+        with open("best_times.csv", "r", newline="") as csvfile:
+            csv_reader = csv.reader(csvfile)
+            first = None
+            second = None
+            third= None
+            for time in csv_reader:
+                if first == None:
+                    first = time[0]
+                    continue
+                if time[0] < first:
+                    third = second
+                    second = first
+                    first = time[0]
+                elif time[0] < second:
+                    third = second
+                    second = time[0]
+                elif time[0] < third:
+                    third = time[0]
+        self.Best_Times_Label.setText(f"Top 3 Times:\n{first:>12}\n{second:>12}\n{third:>12}")
         self.Win_Label.show()
         self.Win_Label.setStyleSheet("color: maroon")
         self.Win_Label.setText("Lose!")
         self.__running = False
+
+    def update_timer(self):
+        self.__timer_running = True
+        self.time = self.time.addMSecs(10)
+        self.Timer_Label.setText(f"Time: {self.time.hour():02}:{self.time.minute():02}:{self.time.second():02}")
