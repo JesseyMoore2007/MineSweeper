96952217ed6453dcd73cdba54e6f6a1aed2ba0ab
vicek <jesseymoore2007@gmail.com>
2026-04-20 20:15:00 -0500

    A decent framework for minesweeper

--- logic.py
+++ logic.py
@@ -17,6 +17,7 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.mines:list = []
         self.cleared:int = 0
         self.safe_tiles:int = 22
+        self.tiles_cleared:dict = {}
         self.Flags_Label.setText(f"Flags: {self.__flags}")
 
         first = True
@@ -30,6 +31,7 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.create_grid()
 
 
+
     def create_grid(self) -> None:
         '''
         Sets up the grid lists and the mine locations based on the grid
@@ -37,8 +39,8 @@ class Logic(QMainWindow, Ui_MainWindow):
         '''
         letter = 0
         for row in self.grid:
-            for i in range(5):
-              row.append(self.grid_rows[letter] + (str(i+1)))
+            for col in range(5):
+              row.append(self.grid_rows[letter] + (str(col+1)))
             letter += 1
 
         mine1 = random.choice(self.grid_rows) + random.choice(self.grid_columns)
@@ -49,6 +51,14 @@ class Logic(QMainWindow, Ui_MainWindow):
             mine2 = random.choice(self.grid_rows) + random.choice(self.grid_columns)
             mine3 = random.choice(self.grid_rows) + random.choice(self.grid_columns)
         self.mines = [mine1, mine2, mine3]
+
+        for row in self.grid:
+            for col in row:
+                if col not in self.mines:
+                    self.tiles_cleared[col] = 0
+
+        print(self.tiles_cleared)
+
 
     def click(self) -> None:
         if self.__running:
@@ -63,24 +73,32 @@ class Logic(QMainWindow, Ui_MainWindow):
                     for col in row:
                         if col == tile:
                             print(f"Location: {self.grid.index(row)} {row.index(col)}")
-                            number = str(self.give_number(tile, self.grid.index(row), row.index(col)))
+                            number = str(self.give_number(self.grid.index(row), row.index(col)))
                             print(f"Name: {tile}")
                             self.sender().setStyleSheet("background-color: brown")
                             self.sender().setText(number)
-                            self.cleared += 1
-                            if self.cleared == self.safe_tiles:
-                                self.win()
-                            return
+                            print(number)
+                            self.tiles_cleared[tile] = 1
+                            self.cleared = 0
+                            for value in self.tiles_cleared.values():
+                                if value == 1:
+                                    self.cleared += 1
+                                if self.cleared == self.safe_tiles:
+                                    self.win()
+                                    return
+                            if number == "0":
+                                print("breaking")
+                                self.break_adjacent(self.grid.index(row), row.index(col))
+
                 #print(tile)
                 #button.setStyleSheet("background-color: brown")
                 #button.setText()
                 #return
 
 
-    def give_number(self, tile, row, col):
+    def give_number(self, row, col):
         '''
         This function returns the amount of mines around a clicked tile
-        :param tile:
         :param row:
         :param col:
         :return:
@@ -103,6 +121,31 @@ class Logic(QMainWindow, Ui_MainWindow):
                     number += 1
         print("-----")
         return number
+
+    def break_adjacent(self, row, col):
+        places = [-1, 0, 1]
+        for i in places:
+            if i == -1 and row == 0:
+                continue
+            if i == 1 and row == 4:
+                continue
+            for j in places:
+                if j == -1 and col == 0:
+                    continue
+                if j == 1 and col == 4:
+                    continue
+                #if self.give_number(row, col) == 0 and name not in self.mines:
+                first = True
+                for button in self.gridLayoutWidget.children():
+                    if first:
+                        first = False
+                        continue
+                    print(button.objectName())
+                    print(self.grid[row+i][col+j])
+                    if button.objectName() == self.grid[row+i][col+j] and self.tiles_cleared[button.objectName()] != 1:
+                        print("clicked")
+                        button.click()
+        pass
 
     def win(self):
         '''
